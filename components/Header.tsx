ff6b373f940a07d0c9aefe9544bd052666644c96
takayakawai <takuyakawai.1989@gmail.com>
2021-12-24 21:05:15 +0900

    14th commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import Navbar from './Navbar';
 
 export default function Header() {
   return (
-    <div className='px-2 flex justify-between h-12 items-center md:px-0 md:h-20'>
+    <div className='px-2 flex justify-between h-12 items-center lg:px-0 md:h-20'>
       <div className='relative w-36 h-12 md:w-40'>
         <Link href='/'>
           <a>
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -10,15 +10,13 @@ const Home: NextPage = () => {
             ご予約・お問い合わせ
           </h1>
         </div>
-        <div className='sm:flex justify-center gap-9'>
-          <section className='bg-gray-200 p-4 rounded-lg text-center w-5/12 h-48'>
-            <h2 className='text-md font-semibold text-gray-600 mb-12'>
-              メールからのご連絡はこちら
-            </h2>
-            <p className='font-readex text-2xl'>y0806.a0726@gmail.com</p>
+        <div className='px-4 container flex flex-col sm:flex-row sm:justify-center gap-6 sm:gap-9 sm:px-0'>
+          <section className='bg-gray-200 p-4 rounded-lg text-center w-full sm:w-5/12 h-40 sm:h-48'>
+            <h2 className='text-md text-gray-600 mb-8 sm:mb-12'>メールからのご連絡はこちら</h2>
+            <p className='text-lg font-readex md:text-xl lg:text-2xl'>y0806.a0726@gmail.com</p>
           </section>
-          <section className='bg-gray-200 p-4 rounded-lg text-center w-5/12 h-48'>
-            <h2 className='text-md font-semibold text-gray-600 mb-12'>LINEからのご連絡はこちら</h2>
+          <section className='bg-gray-200 p-4 rounded-lg text-center w-full sm:w-5/12 h-40 sm:h-48'>
+            <h2 className='text-md text-gray-600 mb-8 sm:mb-12'>LINEからのご連絡はこちら</h2>
             <a
               href='http://line.me/ti/p/%40321wmfzo'
               className='bg-black text-white text-md tracking-wide rounded-full h-10 w-48 mx-auto flex items-center text-center justify-center hover:bg-red-500 duration-200'
