c24b69fca769d4aeee7b042e2b84721ad0002f76
takayakawai <takuyakawai.1989@gmail.com>
2021-12-24 21:19:49 +0900

    14.5th commit

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -11,11 +11,11 @@ const Home: NextPage = () => {
           </h1>
         </div>
         <div className='px-4 container flex flex-col sm:flex-row sm:justify-center gap-6 sm:gap-9 sm:px-0'>
-          <section className='bg-gray-200 p-4 rounded-lg text-center w-full sm:w-5/12 h-40 sm:h-48'>
+          <section className='bg-gray-200 p-4 rounded-lg text-center w-full sm:w-6/12 h-40 sm:h-48'>
             <h2 className='text-md text-gray-600 mb-8 sm:mb-12'>メールからのご連絡はこちら</h2>
-            <p className='text-lg font-readex md:text-xl lg:text-2xl'>y0806.a0726@gmail.com</p>
+            <p className='text-xl font-readex sm:text-md lg:text-2xl'>y0806.a0726@gmail.com</p>
           </section>
-          <section className='bg-gray-200 p-4 rounded-lg text-center w-full sm:w-5/12 h-40 sm:h-48'>
+          <section className='bg-gray-200 p-4 rounded-lg text-center w-full sm:w-6/12 h-40 sm:h-48'>
             <h2 className='text-md text-gray-600 mb-8 sm:mb-12'>LINEからのご連絡はこちら</h2>
             <a
               href='http://line.me/ti/p/%40321wmfzo'
